aee246ced74b7d7a7c9ff1767256dbecaf3cf49c
francescoMain <cesaranofrancescomain@gmail.com>
2023-02-06 22:16:10 +0100

    created route delete

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MainController;
 
-Route::get('/', [MainController :: class, 'home']);
+Route::get('/', [MainController :: class, 'home'])
+    ->name('home');
+
 
 Route :: get('/person/show/{person}', [MainController :: class, 'personShow'])
     -> name('person.show');
+
+Route :: get('/person/delete/{person}', [MainController :: class, 'personDelete'])
+    -> name('person.delete');
