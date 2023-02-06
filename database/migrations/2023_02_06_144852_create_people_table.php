d703fb81877479992dd063499a90a3955272248a
francescoMain <cesaranofrancescomain@gmail.com>
2023-02-06 20:10:18 +0100

    variable of people

--- database/migrations/2023_02_06_144852_create_people_table.php
+++ database/migrations/2023_02_06_144852_create_people_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('people', function (Blueprint $table) {
             $table->id();
 
-            $table->string("fristName", 32);
-            $table->string("lastName", 32);
-            $table->date("dateOfBirth");
-            $table->integer("heigth", 32) -> unsigned() -> nullable ();
+            $table->string('fristName', 32);
+            $table->string('lastName', 32);
+            $table->date('dateOfBirth');
+            $table->integer('heigth') -> unsigned() -> nullable();
             
             $table->timestamps();
         });
