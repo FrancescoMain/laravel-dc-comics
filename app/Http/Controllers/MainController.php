441ac091942348c4e60c5c8202dd6c5a94bf1480
francescoMain <cesaranofrancescomain@gmail.com>
2023-02-06 21:56:16 +0100

    created function home

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Person;
+
 class MainController extends Controller
 {
+    
     public function home() {
 
-        return view('pages.home');
+        $people = Person :: all();
+
+
+        return view('pages.home', compact('people'));
     }
 }
