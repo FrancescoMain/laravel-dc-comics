16f3180b27c9e2f76d8353736f3764e1fca61f17
francescoMain <cesaranofrancescomain@gmail.com>
2023-02-06 22:30:11 +0100

    created functino person create && person store

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -28,4 +28,30 @@ class MainController extends Controller
     
         return redirect() -> route('home');
     }
+
+    public function personCreate() {
+
+        return view('pages.personCreate');
+    }
+
+    public function personStore(Request $request) {
+
+        $data = $request -> validate([
+            'fristName' => 'required|string|max:32',
+            'lastName' => 'required|string|max:32',
+            'dateOfBirth' => 'date',
+            'heigth' => 'nullable|integer|min:150|max:220',
+        ]);
+    
+        $person = new Person();
+    
+        $person -> fristName = $data['fristName'];
+        $person -> lastName = $data['lastName'];
+        $person -> dateOfBirth = $data['dateOfBirth'];
+        $person -> heigth = $data['heigth'];
+    
+        $person -> save();
+    
+        return redirect() -> route('home');
+    }
 }
